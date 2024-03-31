10c5c25c012dccb8d95c8c65f86f63ba459bd842
Ayarachidi <aya.rachidi02@gmail.com>
2024-03-31 17:54:16 +0000

    gestionnaire

--- src/main/java/Entities/Gestionnaire.java
+++ src/main/java/Entities/Gestionnaire.java
@@ -6,28 +6,20 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class SuperAdmin {
-    @Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Integer idadmin;
+public class Gestionnaire {
+    @Id  @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer idgestionnaire;
     private String nom;
     private String prenom;
     private String email;
     private String tele;
     @OneToOne
-    private Login emailadmin;
-    @OneToMany(mappedBy = "superadmin")
-    private List<Gestionnaire> gestionnaires;
-
-
-
-
-
-
+    private Login emailgestionnaire;
+    @ManyToOne
+    private SuperAdmin superadmin;
 }
