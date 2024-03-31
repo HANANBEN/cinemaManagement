f9f00e9f8f2fe95f58071e2b3914edfb464c4ec8
Ayarachidi <aya.rachidi02@gmail.com>
2024-03-31 23:10:24 +0000

    Avis

--- src/main/java/Entities/Avis.java
+++ src/main/java/Entities/Avis.java
@@ -1,31 +1,23 @@
 package Entities;
 
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Entity
-public class Film {
+public class Avis {
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
-    private int idfilm;
-
-    private String titre;
-    @OneToMany(mappedBy = "film")
-    private List<Projectionfilm> projectionfilms;
-    @OneToMany(mappedBy = "film")
-    private List<Avis> Avis;
-
-
-
-
-
+    private Integer idavis;
+    @ManyToOne
+    private Client client;
+    @ManyToOne
+    private Film film;
 }
