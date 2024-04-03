7aa6e6a237d70447e218a1a7e49fd27e162304f3
HANANBEN <hbenazize@gmail.com>
2024-04-03 16:16:06 +0000

    commit changes

--- src/main/java/Repositories/GestionnaireRepository.java
+++ src/main/java/Repositories/GestionnaireRepository.java
@@ -2,6 +2,8 @@ package Repositories;
 
 import Entities.Gestionnaire;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource
 public interface GestionnaireRepository  extends JpaRepository<Gestionnaire ,Integer> {
 }
