8ca090bef4fc335305addaa6fb820b23dc5aeeac
Ayarachidi <aya.rachidi02@gmail.com>
2024-04-15 08:53:19 +0100

    modification

--- src/main/java/Controller/controllercinema.java
+++ src/main/java/Controller/controllercinema.java
@@ -4,10 +4,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import Services.CinemaService;
+
 
 @Controller
 public class controllercinema {
-
+	
+     private CinemaService cinema;
 	@GetMapping("/hh")
      public String get(Model model){
          model.addAttribute("e", "hhh");
